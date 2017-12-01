fabe6701582f945186d7c3fc7b2f361774992f92
Maros Kopec <maroskopec@outlook.com>
2017-12-01 08:18:23 +0100

    Koa example

---
+++ index.js
@@ -0,0 +1,35 @@
+const Koa = require('koa');
+const app = new Koa();
+
+var nano = require('nano')('http://localhost:5984');
+const compose = require('koa-compose');
+
+async function random(ctx, next) {
+  if ('/random' == ctx.path) {
+    ctx.body = Math.floor(Math.random() * 10);
+  } else {
+    await next();
+  }
+};
+
+async function backwards(ctx, next) {
+  if ('/backwards' == ctx.path) {
+    ctx.body = 'sdrawkcab';
+  } else {
+    await next();
+  }
+}
+
+async function pi(ctx, next) {
+  if ('/pi' == ctx.path) {
+    ctx.body = String(Math.PI);
+  } else {
+    await next();
+  }
+}
+
+const all = compose([random, backwards, pi]);
+
+app.use(all);
+app.listen(8080);
+
